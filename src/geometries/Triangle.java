9642f96e20f4211a41eaeb72a1bfbc9ffc9ffd51
alishtren11@gmail.com <alishtren11@gmail.com>
2025-04-06 20:18:45 +0300

    fixing tringal

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -8,6 +8,13 @@ import primitives.Vector;
  */
 public class Triangle extends Polygon {
 
+    /**
+     * Constructs a triangle from three points.
+     *
+     * @param p1 the first vertex of the triangle
+     * @param p2 the second vertex of the triangle
+     * @param p3 the third vertex of the triangle
+     */
     public Triangle(Point p1, Point p2, Point p3) {
         super(p1, p2, p3);
     }
